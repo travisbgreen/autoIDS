81f8ff6014571793c6cc5e00849ba3171c95f818
red-green <nwx@protonmail.com>
2017-07-27 12:06:39 -0600

    Changed the execution method to remove bash from the equation, also finished the custom rule stuff.
    Should be the answer to #20 and #7, but not tested yet.... Requires my IDSDB until the pull request is done: https://github.com/red-green/IDSDeathBlossom

--- background.py
+++ background.py
@@ -1,11 +1,14 @@
 from config import *
-import threading, os, time ### TODO: time the operation and save in db
+import threading, os, time, sys, tempfile
 from peewee import *
 
 db = SqliteDatabase(DATABASE, threadlocals=True)
 
 threadlock = threading.Lock() # make sure we don't have any threading issues
 datalock = threading.Lock()
+
+sys.path.append(IDSDB_FOLDER)
+from IDSDeathBlossom import run as run_ids
 
 def backgroundthread(run): # runs in the background and processes files one at a time
 	global threadlock,datalock
@@ -14,11 +17,27 @@ def backgroundthread(run): # runs in the background and processes files one at a
 		logpath = os.path.join(LOG_FOLDER,run.pcap.md5+run.runid) # make a folder in the log directory that is tagged with a long hash
 		os.mkdir(logpath) # make a new subdirectory for the logs of this run to be stored in
 		os.chdir(IDSDB_FOLDER) # cd to the path of the IDSDeathBlossom install so all the relative links and imports work properly
+		rulesfile = tempfile.NamedTemporaryFile(delete=True) # make a temporary file for the rules if it has them
+		class temp_config: # passed to IDSDB handler
+			runmode = 'run' # what else would you want to do?
+			config = '{IDSDB}/config/config.yaml'.format(IDSDB=IDSDB_FOLDER) # default config
+			targets = '{IDS}-{ENGINE}'.format(IDS=run.ids,ENGINE=run.engine) # set the ids and ruleset
+			pcappath = run.pcap.filepath # path to the pcap to be executed
+			globallogdir = logpath # point to our special logging path
+			glogoverride = True # i guess this means send all logs to that folder
+			reporton = 'ids,fast' # things to report about
+		if run.engine == 'test-test': # custom rules
+			rulesfile.write(run.rules) # write the rules to our temp file
+			rulesfile.flush() # save without closing because that deletes the file
+			temp_config.usecustomrules = True # tell it to use our rules
+			temp_config.target-opts = 'all:customrules="{RULEFILE}"'.format(rulesfile.name) # point it at our file
 		threadlock.acquire(True) # make sure we don't run these at the same time
 		starttime = time.clock()
-		ret = os.system('''python {IDSDB}/IDSDeathBlossom.py -c {IDSDB}/config/config.yaml -R run -t "{IDS}-{ENGINE}" --pcappath="{PCAP}" --globallogdir={LOGPATH} --glogoverride --reporton=ids,fast'''.format(IDSDB=IDSDB_FOLDER,IDS=run.ids,ENGINE=run.engine,PCAP=run.pcap.filepath,LOGPATH=logpath))  ## --use-custom-rules  --target-opts="all:customrules='.$rulesfile.'"
+		ret = run_ids(temp_config) # runs the program directly without bash
+		#ret = os.system('''python {IDSDB}/IDSDeathBlossom.py -c {IDSDB}/config/config.yaml -R run -t "{IDS}-{ENGINE}" --pcappath="{PCAP}" --globallogdir={LOGPATH} --glogoverride --reporton=ids,fast'''.format(IDSDB=IDSDB_FOLDER,IDS=run.ids,ENGINE=run.engine,PCAP=run.pcap.filepath,LOGPATH=logpath))  ## --use-custom-rules  --target-opts="all:customrules='.$rulesfile.'"
 		endtime = time.clock()
 		threadlock.release() # this lock only prevents IDSdb from running multiple times at once
+		rulesfile.close() # delete the temp file
 		stat = 1 # success
 		if ret != 0:
 			stat = -1 # fail
